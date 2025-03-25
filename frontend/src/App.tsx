4506408e4326e29b714c7f63afca9291bef0dffa
nitro56565 <nitro56565@gmail.com>
2025-03-25 16:31:31 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import ConcertoEditor from './pages/ctoEditor';
-import FileTextExtractor from './pages/upload';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ConcertoEditor from "./pages/ctoEditor";
+import FileTextExtractor from "./pages/upload";
 
 function App() {
   return (
