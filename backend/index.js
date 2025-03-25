da757ec838425364f5b870f795f8a280c9038565
nitro56565 <nitro56565@gmail.com>
2025-03-25 18:06:36 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,29 @@ import cors from 'cors';
 import express from 'express';
 import { CompletionCopilot } from 'monacopilot';
 
+const API_KEY = process.env.MISTRAL_API_KEY
+
 const app = express();
 app.use(cors(
-    { origin: 'http://localhost:5173' }
+    { origin: 'https://ai-autocomplete-suggestion.onrender.com' }
 ));
 app.use(express.json());
 
-const copilot = new CompletionCopilot(process.env.MISTRAL_API_KEY, {
+const copilot = new CompletionCopilot(API_KEY, {
     provider: 'mistral',
     model: 'codestral',
 });
 
 app.post('/api/code-completion', async (req, res) => {
-    const completion = await copilot.complete({ body: req.body });
-
-    res.json(completion);
+    try {
+        const completion = await copilot.complete({ body: req.body });
+        res.json(completion);
+    } catch (error) {
+        console.error("Error during code completion:", error);
+        res.status(500).json({ error: "Failed to complete code. Please check your API key and configuration." });
+    }
 });
+
 
 app.get('/', (req, res) => {
     res.send('Code Companion Server');
